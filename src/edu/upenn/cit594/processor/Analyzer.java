cf402371b6789cde0256fac721e439e1b88bbd44
eguglielmelli <117960955+eguglielmelli@users.noreply.github.com>
2023-03-29 07:08:55 -0700

    Placeholder methods for our data analyzing class (minus custom feature)

--- src/edu/upenn/cit594/processor/Analyzer.java
+++ src/edu/upenn/cit594/processor/Analyzer.java
@@ -1,5 +1,23 @@
 package edu.upenn.cit594.processor;
 
+
 public class Analyzer {
+
+    public void totalPopulation() {
+
+    }
+    public void vaccinationsPerCapita() {
+
+    }
+    public void averagePropertyValue(String ZIP) {
+
+    }
+    public void averageTotalLivableArea(String ZIP) {
+
+    }
+    public void totalMarketValuePerCapita(String ZIP) {
+
+    }
+
 
 }
