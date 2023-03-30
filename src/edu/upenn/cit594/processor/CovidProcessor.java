d5a3eda1cbb62b1ed50ea08840c9bd7c12cdc32d
eguglielmelli <117960955+eguglielmelli@users.noreply.github.com>
2023-03-30 13:35:42 -0700

    implemented memoization for the vaccinationpercapita

--- src/edu/upenn/cit594/processor/CovidProcessor.java
+++ src/edu/upenn/cit594/processor/CovidProcessor.java
@@ -1,6 +1,7 @@
 package edu.upenn.cit594.processor;
 
 import java.text.DecimalFormat;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -10,33 +11,37 @@ import edu.upenn.cit594.util.Zip;
 public class CovidProcessor {
 	
 	private TreeMap<String, Zip> data = null;
+	private HashMap<String,TreeMap<Integer,Double>> memoTable = new HashMap<>();
 	
     public CovidProcessor(TreeMap<String, Zip> data) {
     	this.data = data;
 	}
 
-    // Check memoization techniques (Module 13). Might need to refactor...
-	public TreeMap<Integer, Double> vaccinationsPerCapita(String choice, String date) {
+
+    // Memoization may need a little more work, will revisit this one
+	public TreeMap<Integer,Double> vaccinationsPerCapita(String choice, String date) {
+		if(memoTable.containsKey(choice)) {
+			return memoTable.get(choice);
+		}
 		TreeMap<Integer, Double> vaccinations = new TreeMap<Integer, Double>();
 		DecimalFormat rounder = new DecimalFormat("#.0000");
-		
         for(Map.Entry<String, Zip> entry : data.entrySet()) {
         	int zip = Integer.parseInt(entry.getKey());
         	int population = entry.getValue().getTotalPopulation();
         	Covid cases = entry.getValue().getCovidCasesForDate(date);
-        	
+
         	if(population == -1 || cases == null) {continue;}
-        		
+
         	int totalVaccinated = 0;
         	if(choice.equals("full")) {totalVaccinated = cases.getFullyVaccinated();}
         	else if (choice.equals("partial")) {totalVaccinated = cases.getPartiallyVaccinated();}
-        	        	
+
         	if(totalVaccinated == 0) {continue;}
-        	
+
         	double perCapita = (double) totalVaccinated/population;
             vaccinations.put(zip, Double.parseDouble(rounder.format(perCapita)));
           }
-        
+		memoTable.put(choice,vaccinations);
 		return vaccinations;
     }
 	
