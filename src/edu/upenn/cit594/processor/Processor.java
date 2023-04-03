21862855200b6028557be5ede153b5a837f8743a
eguglielmelli <117960955+eguglielmelli@users.noreply.github.com>
2023-04-02 21:10:03 -0700

    some changes made to the available actions and new processor constructor

--- src/edu/upenn/cit594/processor/Processor.java
+++ src/edu/upenn/cit594/processor/Processor.java
@@ -1,10 +1,13 @@
 package edu.upenn.cit594.processor;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
 
 import edu.upenn.cit594.datamanagement.Reader;
+import edu.upenn.cit594.util.Zip;
 
 public class Processor {
 	
@@ -13,13 +16,35 @@ public class Processor {
     CovidProcessor covidProcessor;
     PropertyProcessor propertyProcessor;
 
-    public Processor(Reader reader) {
+    public Processor(Reader reader) throws Exception {
+        availableActions = new ArrayList<>();
+        availableActions.add(0);
+        availableActions.add(1);
+        TreeMap<String, Zip> data = reader.getData();
         boolean[] readFiles = reader.getReadFiles();
-        if(readFiles[1]) {populationProcessor = new PopulationProcessor(reader.getData());}
-        if(readFiles[2]) {covidProcessor = new CovidProcessor(reader.getData());}
-        if(readFiles[3]) {propertyProcessor = new PropertyProcessor(reader.getData());}
+        if(readFiles[1]) {
+            populationProcessor = new PopulationProcessor(data);
+            availableActions.add(2);
+        }
+        if(readFiles[2]) {covidProcessor = new CovidProcessor(data);}
+
+        if(readFiles[3]) {
+            propertyProcessor = new PropertyProcessor(data);
+            availableActions.add(4);
+            availableActions.add(5);
+        }
+        if(readFiles[1] && readFiles[2]) {
+            if(!availableActions.contains(2)) {
+                availableActions.add(2);
+            }
+            availableActions.add(3);
+        }
+        if(readFiles[1] && readFiles[3]) {
+            availableActions.add(6);
+        }
+        Collections.sort(availableActions);
     }
-    
+
 
     public List<Integer> getAvailableActions() {return availableActions;}
     
