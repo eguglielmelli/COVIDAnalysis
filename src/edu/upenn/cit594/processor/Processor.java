0e59184411741001360e90dc7bc76a914d4e9711
eguglielmelli <117960955+eguglielmelli@users.noreply.github.com>
2023-04-12 18:21:57 -0700

    new method that finds the closest dates to the given dates for our additional feature, updated test file accordingly

--- src/edu/upenn/cit594/processor/Processor.java
+++ src/edu/upenn/cit594/processor/Processor.java
@@ -1,10 +1,10 @@
 package edu.upenn.cit594.processor;
-
-
 import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.*;
-
 import edu.upenn.cit594.datamanagement.Reader;
+import edu.upenn.cit594.util.Covid;
 import edu.upenn.cit594.util.Zip;
 
 public class Processor {
@@ -16,7 +16,7 @@ public class Processor {
     TreeMap<String,Zip> data = null;
     HashMap<String,TreeMap<String, HashMap<Integer,Double>>> memoTable = new HashMap<>();
 
-    public Processor(Reader reader) throws Exception {
+    public Processor(Reader reader) {
         availableActions = new ArrayList<>();
         availableActions.add(0);
         availableActions.add(1);
@@ -63,15 +63,78 @@ public class Processor {
     
     public int getTotalMarketValuePerCapita(String zipCode) {return propertyProcessor.totalMarketValuePerCapita(zipCode);}
 
+    public String[] findClosestDates(String startDate,String endDate,String zipCode) {
+        String[] dateArray = new String[2];
+        Covid startCovid = data.get(zipCode).getCovidCases().getOrDefault(startDate, null);
+        Covid endCovid = data.get(zipCode).getCovidCases().getOrDefault(endDate, null);
+        LocalDate beginDate;
+        LocalDate endingDate;
+        try {
+            beginDate = LocalDate.parse(startDate);
+            endingDate = LocalDate.parse(endDate);
+        }catch(DateTimeParseException e) {
+            return null;
+        }
+        if (startCovid == null && endCovid != null) {
+            while (startCovid == null) {
+                beginDate = beginDate.plusDays(1);
+                String newStartDate = beginDate.toString();
+                startCovid = data.get(zipCode).getCovidCases().getOrDefault(newStartDate, null);
+                if (beginDate.compareTo(endingDate) == 0) return null;
+            }
+            dateArray[0] = beginDate.toString();
+            dateArray[1] = endDate;
+            return dateArray;
+        }else if (endCovid == null && startCovid != null) {
+            while (endCovid == null) {
+                endingDate = endingDate.minusDays(1);
+                String newEndDate = endingDate.toString();
+                endCovid = data.get(zipCode).getCovidCases().getOrDefault(newEndDate, null);
+                if (endingDate.compareTo(beginDate) == 0) return null;
+            }
+            dateArray[0] = startDate;
+            dateArray[1] = endingDate.toString();
+            return dateArray;
+        }else if (startCovid == null && endCovid == null) {
+            while (startCovid == null || endCovid == null) {
+                if (startCovid == null) {
+                    beginDate = beginDate.plusDays(1);
+                    String newStartDate = beginDate.toString();
+                    Covid newStartCovid = data.get(zipCode).getCovidCases().getOrDefault(newStartDate, null);
+                    if (newStartCovid != null) {
+                        startCovid = newStartCovid;
+                    }
+                }
+                if (endCovid == null) {
+                    endingDate = endingDate.minusDays(1);
+                    String newEndDate = endingDate.toString();
+                    Covid newEndCovid = data.get(zipCode).getCovidCases().getOrDefault(newEndDate, null);
+                    if (newEndCovid != null) {
+                        endCovid = newEndCovid;
+                    }
+                }
+                if (beginDate.compareTo(endingDate) == 0 || beginDate.compareTo(endingDate) > 0) return null;
+            }
+            dateArray[0] = beginDate.toString();
+            dateArray[1] = endingDate.toString();
+            return dateArray;
+        }else {
+            dateArray[0] = startDate;
+            dateArray[1] = endDate;
+            return dateArray;
+        }
+    }
     public TreeMap<String, HashMap<Integer,Double>> getVaccinationIncreaseForDate(String startDate, String endDate) {
+        TreeMap<String,HashMap<Integer,Double>>marketValueVaxxIncrease = new TreeMap<>();
+        if(startDate.compareTo(endDate) == 0 || startDate.compareTo(endDate) > 0 || startDate.compareTo("2022-11-07") >= 0 || endDate.compareTo("2021-03-25") <= 0) return marketValueVaxxIncrease;
         DecimalFormat rounder = new DecimalFormat("#.00");
         String key = startDate + "-" + endDate;
         if(memoTable.containsKey(key)) return memoTable.get(key);
-        TreeMap<String,HashMap<Integer,Double>>marketValueVaxxIncrease = new TreeMap<>();
         for(String zip : data.keySet()) {
             HashMap<Integer,Double> map = new HashMap<>();
-            if(data.get(zip).getCovidCases() != null) {
-                double increaseVaccinated = covidProcessor.vaccinationIncrease(startDate, endDate, zip);
+            String[] array = findClosestDates(startDate,endDate,zip);
+            if(array != null) {
+                double increaseVaccinated = covidProcessor.vaccinationIncrease(array[0], array[1], zip);
                 int marketValuePerCapita = propertyProcessor.totalMarketValuePerCapita(zip);
                 if(marketValuePerCapita != 0 && increaseVaccinated > 0) {
                     map.put(marketValuePerCapita,Double.parseDouble(rounder.format(increaseVaccinated)));
--- src/edu/upenn/cit594/processor/ProcessorTest.java
+++ src/edu/upenn/cit594/processor/ProcessorTest.java
@@ -1,10 +1,7 @@
 package edu.upenn.cit594.processor;
-
 import edu.upenn.cit594.datamanagement.Reader;
 import edu.upenn.cit594.util.Zip;
 import org.junit.jupiter.api.Test;
-
-import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
@@ -57,25 +54,131 @@ class ProcessorTest {
         Reader reader = new Reader(arguments);
         Processor processor = new Processor(reader);
         TreeMap<String,Zip> data = reader.getData();
+        CovidProcessor covidProcessor = new CovidProcessor(data);
 
+        //test case with two invalid dates, we will call closestdates, manually compute increase, and then check map too
+        //after running closest dates, the dates should be 2021-03-25, and 2022-11-07
+        String startDate = "2010-03-25";
+        String endDate = "2023-05-01";
+        String zip = "19102";
+        String[] array = processor.findClosestDates(startDate,endDate,zip);
+        assertEquals("2021-03-25",array[0]);
+        assertEquals("2022-11-07",array[1]);
+        double fullvaxxStart = data.get(zip).getCovidCasesForDate(array[0]).getFullyVaccinated();
+        double fullvaxEnd = data.get(zip).getCovidCasesForDate(array[1]).getFullyVaccinated();
+        double result = ((fullvaxEnd-fullvaxxStart)/fullvaxxStart)*100;
+        double increase = covidProcessor.vaccinationIncrease(array[0],array[1],zip);
+        TreeMap<String,HashMap<Integer,Double>> map = processor.getVaccinationIncreaseForDate(array[0],array[1]);
+        int mvPC = processor.getTotalMarketValuePerCapita(zip);
+        double actualVaxIncrease = map.get(zip).get(mvPC);
+        assertEquals(increase,result,actualVaxIncrease);
 
-        //test case with two valid dates
-        TreeMap<String, HashMap<Integer,Double>> map = processor.getVaccinationIncreaseForDate("2021-03-25","2021-05-01");
-        double firstVax = data.get("19102").getCovidCases().get("2021-03-25").getFullyVaccinated();
-        double secondVax = data.get("19102").getCovidCases().get("2021-05-01").getFullyVaccinated();
-        double increase = ((secondVax-firstVax)/firstVax)*100;
-        DecimalFormat rounder = new DecimalFormat("#.00");
-        increase = Double.parseDouble(rounder.format(increase));
-        int marketValPerCap = processor.getTotalMarketValuePerCapita("19102");
-        assertTrue(map.get("19102").get(marketValPerCap) == increase);
-
-
-        //test case with one valid date and one not, this should return an empty map because the data cannot be computed
-        map = processor.getVaccinationIncreaseForDate("2021-03-25","2024-05-01");
+        //valid case where the two dates have no underlying data in between them map is empty
+        startDate = "2022-05-24";
+        endDate = "2022-05-29";
+        map = processor.getVaccinationIncreaseForDate(startDate,endDate);
         assertTrue(map.isEmpty());
 
-        //test case with two valid dates (within our range) but one doesn't have data, so map should be empty
-        map = processor.getVaccinationIncreaseForDate("2021-03-25","2021-05-19");
+        //another valid test case to make sure computation is correct
+        startDate = "2021-02-20";
+        endDate = "2022-07-01";
+        zip = "19115";
+        map = processor.getVaccinationIncreaseForDate(startDate,endDate);
+        String[] dates = processor.findClosestDates(startDate,endDate,"19115");
+        double vaxxIncrease = covidProcessor.vaccinationIncrease(dates[0],dates[1],zip);
+        int mvpc = processor.getTotalMarketValuePerCapita(zip);
+        result = map.get(zip).get(mvpc);
+        vaxxIncrease = Math.round(vaxxIncrease);
+        result = Math.round(result);
+        assertEquals(result,vaxxIncrease);
+
+        //test simple case where start date and end date are same
+        map = processor.getVaccinationIncreaseForDate("2021-03-25","2021-03-25");
         assertTrue(map.isEmpty());
+
+        //test simple case where end date before start date
+        map = processor.getVaccinationIncreaseForDate("2022-05-01","2021-03-25");
+        assertTrue(map.isEmpty());
+
+        //test simple case where both dates are out of range
+        map = processor.getVaccinationIncreaseForDate("2023-01-01","2024-01-01");
+        assertTrue(map.isEmpty());
+
+        //test simple case with invalid date ie 2021-02-30
+        map = processor.getVaccinationIncreaseForDate("2022-02-40","2022-03-25");
+        assertTrue(map.isEmpty());
+
+    }
+    @Test
+    void findClosestDates() throws Exception {
+        String[] arguments = {"--population=population.csv",
+                "--covid=covid_data.csv", "--properties=downsampled_properties.csv"};
+        Reader reader = new Reader(arguments);
+        Processor processor = new Processor(reader);
+        TreeMap<String, Zip> data = reader.getData();
+        CovidProcessor covidProcessor = new CovidProcessor(data);
+        //test case with zip 19147, start of data for this zip is 3/25/2021, and date closest to end date is 2/11/2022
+        //case where both start and end date are null, so we need to adjust both pointers
+        String startDate = "2021-02-25";
+        String endDate = "2022-02-13";
+        String zipCode = "19147";
+        String[] array = processor.findClosestDates(startDate, endDate, zipCode);
+        assertEquals("2021-03-25",array[0]);
+        assertEquals("2022-02-11",array[1]);
+
+        //case where start date is null and end date is not null, start date should be adjusted to 3/25/2021
+        startDate = "2020-01-10";
+        endDate = "2021-03-27";
+        zipCode = "19147";
+        array = processor.findClosestDates(startDate, endDate, zipCode);
+        assertEquals("2021-03-25",array[0]);
+        assertEquals("2021-03-27",array[1]);
+
+        //case where end date is null and needs to be moved up until valid, end date should be adjusted to 11/07/2022
+        startDate = "2022-05-01";
+        endDate = "2023-11-12";
+        zipCode = "19147";
+        array = processor.findClosestDates(startDate, endDate, zipCode);
+        assertEquals("2022-05-01",array[0]);
+        assertEquals("2022-11-07",array[1]);
+
+        //both start and end are null, and difference is years (20 year span), start date should be 03/25/2021, end date 11/07/2022
+        //this includes the entire data set
+        startDate = "2010-01-10";
+        endDate = "2030-12-10";
+        zipCode = "19147";
+        array = processor.findClosestDates(startDate, endDate, zipCode);
+        assertEquals("2021-03-25",array[0]);
+        assertEquals("2022-11-07",array[1]);
+
+        //check with a zipcode that has no full vaccinations but still appears with dates in the file
+        startDate = "2021-03-25";
+        endDate = "2021-11-05";
+        zipCode = "19108";
+        array = processor.findClosestDates(startDate, endDate, zipCode);
+        assertEquals("2021-03-25",array[0]);
+        assertEquals("2021-11-05",array[1]);
+
+        //check with a completely null zipcode for these dates
+        startDate = "2021-03-25";
+        endDate = "2022-05-01";
+        zipCode = "19199";
+        array = processor.findClosestDates(startDate,endDate,zipCode);
+        assertEquals(array,null);
+
+        //check with a regular zip code that has data throughout the dates
+        startDate = "2021-02-25";
+        endDate = "2022-11-07";
+        zipCode = "19102";
+        array = processor.findClosestDates(startDate,endDate,zipCode);
+        assertEquals("2021-03-25",array[0]);
+        assertEquals("2022-11-07",array[1]);
+
+        //test case where next closest date for each one ends up being the same date (2022-11-07) so array is null
+        startDate = "2022-11-01";
+        endDate = "2022-11-10";
+        zipCode = "19102";
+        array = processor.findClosestDates(startDate,endDate,zipCode);
+        assertEquals(array,null);
     }
 }
